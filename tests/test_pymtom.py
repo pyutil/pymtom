47e92a8e0b29018acf521dee29219a7f697c9753
pyutil <zvolsky@seznam.cz>
2023-01-19 11:20:54 +0100

    before 1st pypi upload

--- tests/test_pymtom.py
+++ tests/test_pymtom.py
@@ -1,5 +1,32 @@
-from pymtom import __version__
+# I have used a test during development, however it was integrated into a different project
+#   and has required SoapUI for mocking.
+# I add as example (of testing or usage)
 
+# def test_mtom_create():
+#     files = ["/home/mirek/tmp/black.png"]
 
-def test_version():
-    assert __version__ == '0.1.0'
+#     # mock = False
+#     mock = True
+
+#     if mock:
+#         # mocked
+#         client = zeep.Client(
+#             "~/w/pysoap/_other/test.wsdl",
+#             transport=MTOMTransport(files=files)
+#         )
+#         client.service._binding_options["address"] = (
+#             "http://localhost:8088/mockimageIOSOAPBinding"
+#         )
+#     else:
+#         # real service (thats a pain: where to take a open and correct service for development?)
+#         client = zeep.Client(
+#             "https://service.url",
+#             transport=MTOMTransport(files=files),
+#         )
+
+#     params = {
+#         "fileName": "testing_im_sorry.png",
+#         "imageData": "cid:{cid}",
+#     }
+
+#     client.service.upload(**params)
